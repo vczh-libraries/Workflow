4a0e3fd259a77688274fd3b0e86fe96133486932
vczh <vczh@163.com>
2015-09-03 01:44:26 -0700

    ...

--- Source/Analyzer/WfAnalyzer_BuildScope.cpp
+++ Source/Analyzer/WfAnalyzer_BuildScope.cpp
@@ -13,86 +13,6 @@ namespace vl
 			using namespace typeimpl;
 
 /***********************************************************************
-CompleteScopeForClassMember
-***********************************************************************/
-
-			class CompleteScopeForClassMemberVisitor : public Object, public WfDeclaration::IVisitor
-			{
-			public:
-				WfLexicalScopeManager*					manager;
-				Ptr<WfClass>							td;
-				Ptr<WfClassMember>						member;
-
-				CompleteScopeForClassMemberVisitor(WfLexicalScopeManager* _manager, Ptr<WfClass> _td, Ptr<WfClassMember> _member)
-					:manager(_manager)
-					, td(_td)
-					, member(_member)
-				{
-				}
-
-				void Visit(WfNamespaceDeclaration* node)override
-				{
-				}
-
-				void Visit(WfFunctionDeclaration* node)override
-				{
-				}
-
-				void Visit(WfVariableDeclaration* node)override
-				{
-				}
-
-				void Visit(WfClassDeclaration* node)override
-				{
-				}
-
-				static void Execute(WfLexicalScopeManager* manager, Ptr<WfClass> td, Ptr<WfClassMember> member)
-				{
-					CompleteScopeForClassMemberVisitor visitor(manager, td, member);
-					member->declaration->Accept(&visitor);
-				}
-			};
-
-/***********************************************************************
-CompleteScopeForDeclaration
-***********************************************************************/
-
-			class CompleteScopeForDeclarationVisitor : public Object, public WfDeclaration::IVisitor
-			{
-			public:
-				WfLexicalScopeManager*					manager;
-				Ptr<WfDeclaration>						declaration;
-
-				CompleteScopeForDeclarationVisitor(WfLexicalScopeManager* _manager, Ptr<WfDeclaration> _declaration)
-					:manager(_manager)
-					, declaration(_declaration)
-				{
-				}
-
-				void Visit(WfNamespaceDeclaration* node)override
-				{
-				}
-
-				void Visit(WfFunctionDeclaration* node)override
-				{
-				}
-
-				void Visit(WfVariableDeclaration* node)override
-				{
-				}
-
-				void Visit(WfClassDeclaration* node)override
-				{
-				}
-
-				static void Execute(WfLexicalScopeManager* manager, Ptr<WfDeclaration> declaration)
-				{
-					CompleteScopeForDeclarationVisitor visitor(manager, declaration);
-					declaration->Accept(&visitor);
-				}
-			};
-
-/***********************************************************************
 BuildScopeForDeclaration
 ***********************************************************************/
 
@@ -631,28 +551,6 @@ BuildScopeForExpression
 					return visitor.resultScope;
 				}
 			};
-
-/***********************************************************************
-CompleteScope
-***********************************************************************/
-
-			void CompleteScopeForClassMember(WfLexicalScopeManager* manager, Ptr<WfClass> td, Ptr<WfClassMember> member)
-			{
-				CompleteScopeForClassMemberVisitor::Execute(manager, td, member);
-			}
-
-			void CompleteScopeForDeclaration(WfLexicalScopeManager* manager, Ptr<WfDeclaration> declaration)
-			{
-				CompleteScopeForDeclarationVisitor::Execute(manager, declaration);
-			}
-
-			void CompleteScopeForModule(WfLexicalScopeManager* manager, Ptr<WfModule> module)
-			{
-				FOREACH(Ptr<WfDeclaration>, declaration, module->declarations)
-				{
-					CompleteScopeForDeclaration(manager, declaration);
-				}
-			}
 
 /***********************************************************************
 BuildScope
