b8dfc0da4470cb2c0eec4f87bb4f24a7c0b09b87
vczh <vczh@163.com>
2016-01-13 23:09:23 -0800

    Fix wrong indentation when printing interfaces

--- Source/Expression/WfExpression.cpp
+++ Source/Expression/WfExpression.cpp
@@ -1101,10 +1101,10 @@ Print (Declaration)
 				switch (node->kind)
 				{
 				case WfClassKind::Class:
-					writer.WriteString(L"class ");
+					writer.WriteString(indent + L"class ");
 					break;
 				case WfClassKind::Interface:
-					writer.WriteString(L"interface ");
+					writer.WriteString(indent + L"interface ");
 					break;
 				}
 				writer.WriteString(node->name.value);
@@ -1123,7 +1123,7 @@ Print (Declaration)
 				}
 
 				writer.WriteLine(L"");
-				writer.WriteLine(L"{");
+				writer.WriteLine(indent + L"{");
 
 				FOREACH_INDEXER(Ptr<WfClassMember>, member, index, node->members)
 				{
@@ -1145,7 +1145,7 @@ Print (Declaration)
 					writer.WriteLine(L"");
 				}
 
-				writer.WriteString(L"}");
+				writer.WriteLine(indent + L"}");
 				writer.AfterPrint(node);
 			}
 		};
