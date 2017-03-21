5d8a5b221d7e9c3b04bb69f5bd4de74777dde6e0
Zihan Chen <vczh@163.com>
2017-03-21 03:19:24 -0700

    ...

--- Source/Analyzer/WfAnalyzer_ExpandNewCoroutineExpression.cpp
+++ Source/Analyzer/WfAnalyzer_ExpandNewCoroutineExpression.cpp
@@ -6,6 +6,105 @@ namespace vl
 	{
 		namespace analyzer
 		{
+			using namespace collections;
+
+/***********************************************************************
+FindCoroutineAwaredStatementVisitor
+***********************************************************************/
+
+			class FindCoroutineAwaredStatementVisitor : public Object, public WfStatement::IVisitor
+			{
+			public:
+				SortedList<WfStatement*>&				awaredStatements;
+				bool									awared = false;
+
+				FindCoroutineAwaredStatementVisitor(SortedList<WfStatement*>& _awaredStatements)
+					:awaredStatements(_awaredStatements)
+				{
+				}
+
+				bool Call(Ptr<WfStatement> node)
+				{
+					if (!node) return false;
+
+					awared = false;
+					node->Accept(this);
+					if (awared)
+					{
+						awaredStatements.Add(node.Obj());
+					}
+					return awared;
+				}
+
+				void Visit(WfBreakStatement* node)override
+				{
+				}
+
+				void Visit(WfContinueStatement* node)override
+				{
+				}
+
+				void Visit(WfReturnStatement* node)override
+				{
+				}
+
+				void Visit(WfDeleteStatement* node)override
+				{
+				}
+
+				void Visit(WfRaiseExceptionStatement* node)override
+				{
+				}
+
+				void Visit(WfIfStatement* node)override
+				{
+					bool a = Call(node->trueBranch);
+					bool b = Call(node->falseBranch);
+					awared = a || b;
+				}
+
+				void Visit(WfWhileStatement* node)override
+				{
+					awared = Call(node->statement);
+				}
+
+				void Visit(WfTryStatement* node)override
+				{
+					bool a = Call(node->protectedStatement);
+					bool b = Call(node->catchStatement);
+					bool c = Call(node->finallyStatement);
+					awared = a || b || c;
+				}
+
+				void Visit(WfBlockStatement* node)override
+				{
+					bool result = false;
+					FOREACH(Ptr<WfStatement>, stat, node->statements)
+					{
+						bool a = Call(stat);
+						result |= a;
+					}
+					awared = result;
+				}
+
+				void Visit(WfVariableStatement* node)override
+				{
+				}
+
+				void Visit(WfExpressionStatement* node)override
+				{
+				}
+
+				void Visit(WfVirtualStatement* node)override
+				{
+					awared = Call(node->expandedStatement);
+				}
+
+				void Visit(WfCoroutineStatement* node)override
+				{
+					awared = true;
+				}
+			};
 
 /***********************************************************************
 ExpandNewCoroutineExpression
