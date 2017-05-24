0c2bec26ee2dc5c137e70e9823644b9bf9d54dd3
Zihan Chen <vczh@163.com>
2017-05-23 22:42:03 -0700

    ...

--- Source/Cpp/WfCpp_Expression.cpp
+++ Source/Cpp/WfCpp_Expression.cpp
@@ -1030,6 +1030,10 @@ WfGenerateExpressionVisitor
 							{
 								writer.WriteString(L".Obj()");
 							}
+							else if (parentResult.type->GetDecorator() == ITypeInfo::Nullable)
+							{
+								writer.WriteString(L".Value()");
+							}
 							if (isRef) writer.WriteString(L")");
 							return true;
 						}, useReturnValue);
