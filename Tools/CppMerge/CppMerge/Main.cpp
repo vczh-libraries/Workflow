e4e0c300b71940576e3bdcc393f57f781b2ed5a8
Zihan Chen <vczh@163.com>
2017-08-25 00:20:39 -0700

    CppMerge

---
+++ Tools/CppMerge/CppMerge/Main.cpp
@@ -0,0 +1,49 @@
+#include "VlppWorkflowCompiler.h"
+
+using namespace vl;
+using namespace vl::console;
+using namespace vl::filesystem;
+using namespace vl::stream;
+using namespace vl::workflow::cppcodegen;
+
+#if defined VCZH_MSVC
+int wmain(int argc, wchar_t* argv[])
+#elif defined VCZH_GCC
+int main(int argc, char* argv[])
+#endif
+{
+	if (argc != 4)
+	{
+		Console::WriteLine(L"Usage: CppMerge <x32-file> <x64-file> <output-file>");
+		return 0;
+	}
+#if defined VCZH_MSVC
+	FilePath file32 = argv[1];
+	FilePath file64 = argv[2];
+	FilePath fileOutput = argv[3];
+#elif defined VCZH_GCC
+	FilePath path32 = atow(argv[1]);
+	FilePath path64 = atow(argv[2]);
+	FilePath pathOut = atow(argv[3]);
+#endif
+
+	auto code = MergeCppMultiPlatform(File(file32).ReadAllTextByBom(), File(file64).ReadAllTextByBom());
+
+	File file(fileOutput);
+	if (file.Exists())
+	{
+		code = MergeCppFileContent(file.ReadAllTextByBom(), code);
+	}
+
+	if (file.Exists())
+	{
+		auto originalCode = file.ReadAllTextByBom();
+		if (originalCode == code)
+		{
+			return 0;
+		}
+	}
+
+	file.WriteAllText(code, true, BomEncoder::Utf8);
+	return 0;
+}
