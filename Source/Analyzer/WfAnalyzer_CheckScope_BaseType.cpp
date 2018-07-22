a25f3adccbcfdb0e63c45436ff1e5d347a81aa9f
Zihan Chen <vczh@163.com>
2018-07-21 17:29:38 -0700

    Group declaration cycle dependency checking together

--- Source/Analyzer/WfAnalyzer_CheckScope_BaseType.cpp
+++ Source/Analyzer/WfAnalyzer_CheckScope_BaseType.cpp
@@ -50,6 +50,46 @@ CheckBaseClass
 					}
 				}
 
+				void CheckRecursiveStruct(WfStructDeclaration* node, ITypeDescriptor* target, ITypeDescriptor* current, List<WString>& path)
+				{
+					if (target == current)
+					{
+						manager->errors.Add(WfErrors::StructRecursivelyIncludeItself(
+							node,
+							From(path).Aggregate([](const WString& a, const WString& b) {return a + L"::" + b; })
+							));
+						return;
+					}
+
+					if (current == nullptr)
+					{
+						current = target;
+					}
+
+					vint count = current->GetPropertyCount();
+					for (vint i = 0; i < count; i++)
+					{
+						auto prop = current->GetProperty(i);
+						auto propType = prop->GetReturn();
+						auto propTd = prop->GetReturn()->GetTypeDescriptor();
+						if (propType->GetDecorator() == ITypeInfo::TypeDescriptor && propTd->GetTypeDescriptorFlags() == TypeDescriptorFlags::Struct)
+						{
+							vint index = path.Add(prop->GetName());
+							CheckRecursiveStruct(node, target, propTd, path);
+							path.RemoveAt(index);
+						}
+					}
+				}
+
+				void Visit(WfStructDeclaration* node)override
+				{
+					auto scope = manager->nodeScopes[node];
+					auto td = manager->declarationTypes[node].Cast<WfStruct>();
+					List<WString> path;
+					path.Add(td->GetTypeName());
+					CheckRecursiveStruct(node, td.Obj(), nullptr, path);
+				}
+
 				void CheckDuplicatedBaseClass(WfClassDeclaration* node, ITypeDescriptor* td)
 				{
 					List<ITypeDescriptor*> baseTypes;
