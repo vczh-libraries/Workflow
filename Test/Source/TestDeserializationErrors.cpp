dc4ada311c5f5220b5225a4ca8c017aa496f5e9f
Zihan Chen <vczh@163.com>
2019-12-14 05:47:28 -0800

    Update TestDeserializationErrors.cpp

--- Test/Source/TestDeserializationErrors.cpp
+++ Test/Source/TestDeserializationErrors.cpp
@@ -1,8 +1,10 @@
 #include "Helper.h"
 
-TEST_CASE(TestUnresolvedTypesError)
+TEST_FILE
 {
-	const wchar_t* scriptA = LR"(
+	TEST_CASE(L"Test loading assembly with unresolved type")
+	{
+		const wchar_t* scriptA = LR"(
 module A;
 
 class A
@@ -10,56 +12,56 @@ class A
 }
 )";
 
-	const wchar_t* scriptB = LR"(
+		const wchar_t* scriptB = LR"(
 module B;
 
 class B
 {
-  prop a: A^ = null{}
+	prop a: A^ = null{}
 }
 )";
 
-	MemoryStream streamA, streamB;
-	{
-		List<WString> codes;
-		List<Ptr<ParsingError>> errors;
-		codes.Add(scriptA);
-		auto assembly = Compile(GetWorkflowTable(), codes, errors);
-		TEST_ASSERT(assembly);
-		assembly->Serialize(streamA);
-	}
-	{
-		Ptr<WfAssembly> assemblyA;
+		MemoryStream streamA, streamB;
 		{
-			WfAssemblyLoadErrors errors;
-			streamA.SeekFromBegin(0);
-			assemblyA = WfAssembly::Deserialize(streamA, errors);
-			TEST_ASSERT(assemblyA);
-			TEST_ASSERT(errors.unresolvedTypes.Count() == 0);
-			TEST_ASSERT(errors.duplicatedTypes.Count() == 0);
+			List<WString> codes;
+			List<Ptr<ParsingError>> errors;
+			codes.Add(scriptA);
+			auto assembly = Compile(GetWorkflowTable(), codes, errors);
+			TEST_ASSERT(assembly);
+			assembly->Serialize(streamA);
 		}
-		WfRuntimeGlobalContext globalContext(assemblyA);
+		{
+			Ptr<WfAssembly> assemblyA;
+			{
+				WfAssemblyLoadErrors errors;
+				streamA.SeekFromBegin(0);
+				assemblyA = WfAssembly::Deserialize(streamA, errors);
+				TEST_ASSERT(assemblyA);
+				TEST_ASSERT(errors.unresolvedTypes.Count() == 0);
+				TEST_ASSERT(errors.duplicatedTypes.Count() == 0);
+			}
+			WfRuntimeGlobalContext globalContext(assemblyA);
 
-		List<WString> codes;
-		List<Ptr<ParsingError>> errors;
-		codes.Add(scriptB);
-		auto assembly = Compile(GetWorkflowTable(), codes, errors);
-		TEST_ASSERT(assembly);
-		assembly->Serialize(streamB);
-	}
+			List<WString> codes;
+			List<Ptr<ParsingError>> errors;
+			codes.Add(scriptB);
+			auto assembly = Compile(GetWorkflowTable(), codes, errors);
+			TEST_ASSERT(assembly);
+			assembly->Serialize(streamB);
+		}
 
-	WfAssemblyLoadErrors errors;
-	streamB.SeekFromBegin(0);
-	auto assemblyB = WfAssembly::Deserialize(streamB, errors);
-	TEST_ASSERT(!assemblyB);
-	TEST_ASSERT(errors.unresolvedTypes.Count() == 1);
-	TEST_ASSERT(errors.duplicatedTypes.Count() == 0);
-	TEST_ASSERT(errors.unresolvedTypes[0] == L"A");
-}
+		WfAssemblyLoadErrors errors;
+		streamB.SeekFromBegin(0);
+		auto assemblyB = WfAssembly::Deserialize(streamB, errors);
+		TEST_ASSERT(!assemblyB);
+		TEST_ASSERT(errors.unresolvedTypes.Count() == 1);
+		TEST_ASSERT(errors.duplicatedTypes.Count() == 0);
+		TEST_ASSERT(errors.unresolvedTypes[0] == L"A");
+	});
 
-TEST_CASE(TestDuplicatedTypesError)
-{
-	const wchar_t* scriptA = LR"(
+	TEST_CASE(L"Test loading assembly with duplicated types")
+	{
+		const wchar_t* scriptA = LR"(
 module A;
 
 class A
@@ -67,28 +69,29 @@ class A
 }
 )";
 
-	MemoryStream streamA;
-	{
-		List<WString> codes;
-		List<Ptr<ParsingError>> errors;
-		codes.Add(scriptA);
-		auto assembly = Compile(GetWorkflowTable(), codes, errors);
-		TEST_ASSERT(assembly);
-		assembly->Serialize(streamA);
-	}
+		MemoryStream streamA;
+		{
+			List<WString> codes;
+			List<Ptr<ParsingError>> errors;
+			codes.Add(scriptA);
+			auto assembly = Compile(GetWorkflowTable(), codes, errors);
+			TEST_ASSERT(assembly);
+			assembly->Serialize(streamA);
+		}
 
-	WfAssemblyLoadErrors errors;
-	streamA.SeekFromBegin(0);
-	auto assemblyA = WfAssembly::Deserialize(streamA, errors);
-	TEST_ASSERT(assemblyA);
-	TEST_ASSERT(errors.unresolvedTypes.Count() == 0);
-	TEST_ASSERT(errors.duplicatedTypes.Count() == 0);
-	WfRuntimeGlobalContext globalContext(assemblyA);
+		WfAssemblyLoadErrors errors;
+		streamA.SeekFromBegin(0);
+		auto assemblyA = WfAssembly::Deserialize(streamA, errors);
+		TEST_ASSERT(assemblyA);
+		TEST_ASSERT(errors.unresolvedTypes.Count() == 0);
+		TEST_ASSERT(errors.duplicatedTypes.Count() == 0);
+		WfRuntimeGlobalContext globalContext(assemblyA);
 
-	streamA.SeekFromBegin(0);
-	auto assemblyB = WfAssembly::Deserialize(streamA, errors);
-	TEST_ASSERT(!assemblyB);
-	TEST_ASSERT(errors.unresolvedTypes.Count() == 0);
-	TEST_ASSERT(errors.duplicatedTypes.Count() == 1);
-	TEST_ASSERT(errors.duplicatedTypes[0] == L"A");
+		streamA.SeekFromBegin(0);
+		auto assemblyB = WfAssembly::Deserialize(streamA, errors);
+		TEST_ASSERT(!assemblyB);
+		TEST_ASSERT(errors.unresolvedTypes.Count() == 0);
+		TEST_ASSERT(errors.duplicatedTypes.Count() == 1);
+		TEST_ASSERT(errors.duplicatedTypes[0] == L"A");
+	});
 }
