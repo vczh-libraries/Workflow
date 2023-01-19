503400a085ede3dbf0d6236ea05bf71f4b67bcb0
vczh <vczh@outlook.com>
2023-01-18 22:14:24 -0800

    Finished!

--- Source/Analyzer/WfAnalyzer_CheckScope_SymbolType.cpp
+++ Source/Analyzer/WfAnalyzer_CheckScope_SymbolType.cpp
@@ -15,7 +15,22 @@ CheckScopes_SymbolType
 			bool CheckScopes_SymbolType(WfLexicalScopeManager* manager)
 			{
 				vint errorCount = manager->errors.Count();
-				for (auto scope : manager->nodeScopes.Values())
+				for (auto scope : From(manager->nodeScopes)
+					.OrderBy([](auto&& a, auto&& b)
+					{
+						auto rangeA = a.key->codeRange;
+						auto rangeB = b.key->codeRange;
+						if (rangeA.codeIndex != rangeB.codeIndex)
+						{
+							return rangeA.codeIndex - rangeB.codeIndex;
+						}
+						else
+						{
+							return glr::ParsingTextPos::Compare(rangeA.start, rangeB.start);
+						}
+					})
+					.Select([](auto&& pair) { return pair.value; })
+					)
 				{
 					if (!manager->checkedScopes_SymbolType.Contains(scope.Obj()))
 					{
