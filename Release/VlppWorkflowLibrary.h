1ddc3956aded2b9950ba4806eb7204f0a1d01b38
vczh <vczh@outlook.com>
2026-04-18 07:35:35 -0700

    ...

--- Release/VlppWorkflowLibrary.h
+++ Release/VlppWorkflowLibrary.h
@@ -930,7 +930,7 @@ namespace vl
 			, public vl::reflection::Description<IRpcIdSync>
 		{
 		public:
-			virtual void					SyncIds(const vl::collections::Dictionary<vl::WString, vl::vint>& ids) = 0;
+			virtual void					SyncIds(vl::Ptr<vl::reflection::description::IValueDictionary> ids) = 0;
 		};
 
 		class IRpcListOps
@@ -1297,6 +1297,151 @@ Interface Implementation Proxy (Implement)
 #pragma warning(push)
 #pragma warning(disable:4250)
 
+			BEGIN_INTERFACE_PROXY_NOPARENT_SHAREDPTR(vl::rpc_controller::IRpcIdSync)
+				void SyncIds(vl::Ptr<vl::reflection::description::IValueDictionary> ids)override
+				{
+					INVOKE_INTERFACE_PROXY(SyncIds, ids);
+				}
+			END_INTERFACE_PROXY(vl::rpc_controller::IRpcIdSync)
+
+			BEGIN_INTERFACE_PROXY_SHAREDPTR(vl::rpc_controller::IRpcListOps, vl::rpc_controller::IRpcIdSync)
+				vl::rpc_controller::RpcObjectReference EnumCreate(vl::rpc_controller::RpcObjectReference ref)override
+				{
+					INVOKEGET_INTERFACE_PROXY(EnumCreate, ref);
+				}
+
+				bool EnumNext(vl::rpc_controller::RpcObjectReference enumerator)override
+				{
+					INVOKEGET_INTERFACE_PROXY(EnumNext, enumerator);
+				}
+
+				vl::reflection::description::Value EnumGetCurrent(vl::rpc_controller::RpcObjectReference enumerator)override
+				{
+					INVOKEGET_INTERFACE_PROXY(EnumGetCurrent, enumerator);
+				}
+
+				vl::vint ListGetCount(vl::rpc_controller::RpcObjectReference ref)override
+				{
+					INVOKEGET_INTERFACE_PROXY(ListGetCount, ref);
+				}
+
+				vl::reflection::description::Value ListGet(vl::rpc_controller::RpcObjectReference ref, vl::vint index)override
+				{
+					INVOKEGET_INTERFACE_PROXY(ListGet, ref, index);
+				}
+
+				void ListSet(vl::rpc_controller::RpcObjectReference ref, vl::vint index, const vl::reflection::description::Value& value)override
+				{
+					INVOKE_INTERFACE_PROXY(ListSet, ref, index, value);
+				}
+
+				vl::vint ListAdd(vl::rpc_controller::RpcObjectReference ref, const vl::reflection::description::Value& value)override
+				{
+					INVOKEGET_INTERFACE_PROXY(ListAdd, ref, value);
+				}
+
+				vl::vint ListInsert(vl::rpc_controller::RpcObjectReference ref, vl::vint index, const vl::reflection::description::Value& value)override
+				{
+					INVOKEGET_INTERFACE_PROXY(ListInsert, ref, index, value);
+				}
+
+				bool ListRemoveAt(vl::rpc_controller::RpcObjectReference ref, vl::vint index)override
+				{
+					INVOKEGET_INTERFACE_PROXY(ListRemoveAt, ref, index);
+				}
+
+				void ListClear(vl::rpc_controller::RpcObjectReference ref)override
+				{
+					INVOKE_INTERFACE_PROXY(ListClear, ref);
+				}
+
+				bool ListContains(vl::rpc_controller::RpcObjectReference ref, const vl::reflection::description::Value& value)override
+				{
+					INVOKEGET_INTERFACE_PROXY(ListContains, ref, value);
+				}
+
+				vl::vint ListIndexOf(vl::rpc_controller::RpcObjectReference ref, const vl::reflection::description::Value& value)override
+				{
+					INVOKEGET_INTERFACE_PROXY(ListIndexOf, ref, value);
+				}
+
+				vl::vint DictGetCount(vl::rpc_controller::RpcObjectReference ref)override
+				{
+					INVOKEGET_INTERFACE_PROXY(DictGetCount, ref);
+				}
+
+				vl::reflection::description::Value DictGet(vl::rpc_controller::RpcObjectReference ref, const vl::reflection::description::Value& key)override
+				{
+					INVOKEGET_INTERFACE_PROXY(DictGet, ref, key);
+				}
+
+				void DictSet(vl::rpc_controller::RpcObjectReference ref, const vl::reflection::description::Value& key, const vl::reflection::description::Value& value)override
+				{
+					INVOKE_INTERFACE_PROXY(DictSet, ref, key, value);
+				}
+
+				bool DictRemove(vl::rpc_controller::RpcObjectReference ref, const vl::reflection::description::Value& key)override
+				{
+					INVOKEGET_INTERFACE_PROXY(DictRemove, ref, key);
+				}
+
+				void DictClear(vl::rpc_controller::RpcObjectReference ref)override
+				{
+					INVOKE_INTERFACE_PROXY(DictClear, ref);
+				}
+
+				bool DictContainsKey(vl::rpc_controller::RpcObjectReference ref, const vl::reflection::description::Value& key)override
+				{
+					INVOKEGET_INTERFACE_PROXY(DictContainsKey, ref, key);
+				}
+
+				vl::rpc_controller::RpcObjectReference DictGetKeys(vl::rpc_controller::RpcObjectReference ref)override
+				{
+					INVOKEGET_INTERFACE_PROXY(DictGetKeys, ref);
+				}
+
+				vl::rpc_controller::RpcObjectReference DictGetValues(vl::rpc_controller::RpcObjectReference ref)override
+				{
+					INVOKEGET_INTERFACE_PROXY(DictGetValues, ref);
+				}
+			END_INTERFACE_PROXY(vl::rpc_controller::IRpcListOps)
+
+			BEGIN_INTERFACE_PROXY_SHAREDPTR(vl::rpc_controller::IRpcObjectOps, vl::rpc_controller::IRpcIdSync)
+				vl::reflection::description::Value InvokeMethod(vl::rpc_controller::RpcObjectReference ref, vl::vint methodId, vl::Ptr<vl::reflection::description::IValueArray> arguments)override
+				{
+					INVOKEGET_INTERFACE_PROXY(InvokeMethod, ref, methodId, arguments);
+				}
+
+				vl::Ptr<vl::reflection::description::IAsync> InvokeMethodAsync(vl::rpc_controller::RpcObjectReference ref, vl::vint methodId, vl::Ptr<vl::reflection::description::IValueArray> arguments)override
+				{
+					INVOKEGET_INTERFACE_PROXY(InvokeMethodAsync, ref, methodId, arguments);
+				}
+
+				void ObjectHold(vl::rpc_controller::RpcObjectReference ref, bool hold)override
+				{
+					INVOKE_INTERFACE_PROXY(ObjectHold, ref, hold);
+				}
+
+				vl::rpc_controller::RpcObjectReference RequestService(vl::vint typeId)override
+				{
+					INVOKEGET_INTERFACE_PROXY(RequestService, typeId);
+				}
+			END_INTERFACE_PROXY(vl::rpc_controller::IRpcObjectOps)
+
+			BEGIN_INTERFACE_PROXY_SHAREDPTR(vl::rpc_controller::IRpcListEventOps, vl::rpc_controller::IRpcIdSync)
+				void OnItemChanged(vl::rpc_controller::RpcObjectReference ref, vl::vint index, vl::vint oldCount, vl::vint newCount)override
+				{
+					INVOKE_INTERFACE_PROXY(OnItemChanged, ref, index, oldCount, newCount);
+				}
+			END_INTERFACE_PROXY(vl::rpc_controller::IRpcListEventOps)
+
+			BEGIN_INTERFACE_PROXY_SHAREDPTR(vl::rpc_controller::IRpcObjectEventOps, vl::rpc_controller::IRpcIdSync)
+				void InvokeEvent(vl::rpc_controller::RpcObjectReference ref, vl::vint eventId, vl::Ptr<vl::reflection::description::IValueArray> arguments)override
+				{
+					INVOKE_INTERFACE_PROXY(InvokeEvent, ref, eventId, arguments);
+				}
+			END_INTERFACE_PROXY(vl::rpc_controller::IRpcObjectEventOps)
+
 			BEGIN_INTERFACE_PROXY_NOPARENT_SHAREDPTR(ICoroutine)
 
 				void Resume(bool raiseException, Ptr<CoroutineResult> output)override
@@ -1328,47 +1473,6 @@ Interface Implementation Proxy (Implement)
 				}
 			END_INTERFACE_PROXY(IAsync)
 
-			BEGIN_INTERFACE_PROXY_NOPARENT_SHAREDPTR(vl::rpc_controller::IRpcObjectOps)
-
-				void SyncIds(const vl::collections::Dictionary<vl::WString, vl::vint>& ids)override
-				{
-					CHECK_FAIL(L"SyncIds is not supported through proxy.");
-				}
-
-				vl::reflection::description::Value InvokeMethod(vl::rpc_controller::RpcObjectReference ref, vl::vint methodId, vl::Ptr<vl::reflection::description::IValueArray> arguments)override
-				{
-					INVOKEGET_INTERFACE_PROXY(InvokeMethod, ref, methodId, arguments);
-				}
-
-				vl::Ptr<vl::reflection::description::IAsync> InvokeMethodAsync(vl::rpc_controller::RpcObjectReference ref, vl::vint methodId, vl::Ptr<vl::reflection::description::IValueArray> arguments)override
-				{
-					INVOKEGET_INTERFACE_PROXY(InvokeMethodAsync, ref, methodId, arguments);
-				}
-
-				void ObjectHold(vl::rpc_controller::RpcObjectReference ref, bool hold)override
-				{
-					INVOKE_INTERFACE_PROXY(ObjectHold, ref, hold);
-				}
-
-				vl::rpc_controller::RpcObjectReference RequestService(vl::vint typeId)override
-				{
-					INVOKEGET_INTERFACE_PROXY(RequestService, typeId);
-				}
-			END_INTERFACE_PROXY(vl::rpc_controller::IRpcObjectOps)
-
-			BEGIN_INTERFACE_PROXY_NOPARENT_SHAREDPTR(vl::rpc_controller::IRpcObjectEventOps)
-
-				void SyncIds(const vl::collections::Dictionary<vl::WString, vl::vint>& ids)override
-				{
-					CHECK_FAIL(L"SyncIds is not supported through proxy.");
-				}
-
-				void InvokeEvent(vl::rpc_controller::RpcObjectReference ref, vl::vint eventId, vl::Ptr<vl::reflection::description::IValueArray> arguments)override
-				{
-					INVOKE_INTERFACE_PROXY(InvokeEvent, ref, eventId, arguments);
-				}
-			END_INTERFACE_PROXY(vl::rpc_controller::IRpcObjectEventOps)
-
 #pragma warning(pop)
 
 #endif
@@ -1383,4 +1487,3 @@ LoadPredefinedTypes
 }
 
 #endif
-
