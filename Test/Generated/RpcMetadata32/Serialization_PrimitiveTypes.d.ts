a7f34f208fa52b2ffb50c490f3712c42bebd7704
vczh <vczh@outlook.com>
2026-05-02 05:54:11 -0700

    Update generated RPC serialization declarations

---
+++ Test/Generated/RpcMetadata32/Serialization_PrimitiveTypes.d.ts
@@ -0,0 +1,66 @@
+﻿export type PredefinedSchema =
+  | ["UInt8", string]
+  | ["UInt16", string]
+  | ["UInt32", string]
+  | ["UInt64", string]
+  | ["Int8", string]
+  | ["Int16", string]
+  | ["Int32", string]
+  | ["Int64", string]
+  | ["Single", string]
+  | ["Double", string]
+  | ["Boolean", string]
+  | ["Char", string]
+  | ["String", string]
+  | ["DateTime", string]
+  | ["Locale", string]
+  ;
+
+export type EnumSchema =
+  | ["RpcPrimitiveTest::Season", number]
+  ;
+
+export type Schema =
+  | null
+  | PredefinedSchema
+  | EnumSchema
+  | ListSchema
+  | MapSchema
+  | ObservableSchema
+  | StructSchema_RpcPrimitiveTest_Point
+  | StructSchema_system_RpcObjectReference
+  ;
+
+export interface ListSchema<T extends Schema = Schema>
+{
+  "$": "list";
+  values: T[];
+}
+
+export interface MapSchema<K extends Schema = Schema, V extends Schema = Schema>
+{
+  "$": "map";
+  values: Array<[K, V]>;
+}
+
+export interface ObservableSchema<T extends Schema = Schema>
+{
+  "$": "observable-list";
+  values: T[];
+}
+
+export interface StructSchema_RpcPrimitiveTest_Point
+{
+  "$": "RpcPrimitiveTest::Point";
+  x: ["Int32", string];
+  y: ["Int32", string];
+}
+
+export interface StructSchema_system_RpcObjectReference
+{
+  "$": "system::RpcObjectReference";
+  clientId: ["Int32", string];
+  objectId: ["Int32", string];
+  typeId: ["Int32", string];
+}
+
---
+++ Test/Generated/RpcMetadata64/Serialization_PropDefaultInterface.d.ts
@@ -0,0 +1,55 @@
+﻿export type PredefinedSchema =
+  | ["UInt8", string]
+  | ["UInt16", string]
+  | ["UInt32", string]
+  | ["UInt64", string]
+  | ["Int8", string]
+  | ["Int16", string]
+  | ["Int32", string]
+  | ["Int64", string]
+  | ["Single", string]
+  | ["Double", string]
+  | ["Boolean", string]
+  | ["Char", string]
+  | ["String", string]
+  | ["DateTime", string]
+  | ["Locale", string]
+  ;
+
+export type EnumSchema = never;
+
+export type Schema =
+  | null
+  | PredefinedSchema
+  | ListSchema
+  | MapSchema
+  | ObservableSchema
+  | StructSchema_system_RpcObjectReference
+  ;
+
+export interface ListSchema<T extends Schema = Schema>
+{
+  "$": "list";
+  values: T[];
+}
+
+export interface MapSchema<K extends Schema = Schema, V extends Schema = Schema>
+{
+  "$": "map";
+  values: Array<[K, V]>;
+}
+
+export interface ObservableSchema<T extends Schema = Schema>
+{
+  "$": "observable-list";
+  values: T[];
+}
+
+export interface StructSchema_system_RpcObjectReference
+{
+  "$": "system::RpcObjectReference";
+  clientId: ["Int64", string];
+  objectId: ["Int64", string];
+  typeId: ["Int64", string];
+}
+
